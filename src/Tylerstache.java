ba4e5abb1dd19852474e9063055893d36694fc93
root <root@67201097ee49>
2020-06-21 19:54:53 +0000

    game ip

---
+++ src/Tylerstache.java
@@ -0,0 +1,9 @@
+/*
+ * first class would be a class displaying the rules and how to play then space bar to start (copy and paste league invaders)
+ * person running class includes arrow keys 
+ * obstacles class some on the ground and some in the air
+ * class at end where it tells u ur score
+ */
+public class Tylerstache {
+	
+}
