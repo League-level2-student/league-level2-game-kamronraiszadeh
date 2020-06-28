ab07b44070e8afcdbd773621bcdc7b35f75916ce
root <root@5285572c67ae>
2020-06-28 19:56:32 +0000

    level 2 game

--- src/Tylerstache.java
+++ src/Tylerstache.java
@@ -1,3 +1,7 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /*
  * first class would be a class displaying the rules and how to play then space bar to start (copy and paste league invaders)
  * person running class includes arrow keys 
@@ -5,5 +9,17 @@
  * class at end where it tells u ur score
  */
 public class Tylerstache {
+	public static final int WIDTH=500;
+	public static final int HEIGHT = 800;
+	JPanel p = new JPanel();
+	JFrame f = new JFrame();
+	JTextField t = new JTextField();
 	
+	
+
+void setup (){
+	f.setSize(WIDTH,HEIGHT);
+	f.setVisible(true);
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 }
