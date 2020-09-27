8a3c2c67b661707263660480005dc99d79c44416
root <root@06aee389e54e>
2020-09-27 19:57:46 +0000

    i coded

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,13 +1,18 @@
+import java.awt.Color;
 
-public class Obstacle extends GamePanel {
+public class Obstacle  extends GameObject () {
 
 	void update () {
 		
 		
 	}
 	
-	void draw() {
+	void draw(Graphics g) {
 	      g.setColor(Color.YELLOW);
 	        g.fillRect(x, y, 50, 50);
+	}
+	
+	void update () {
+		
 	}
 }
