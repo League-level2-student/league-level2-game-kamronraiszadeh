5170d04f2bdc217e43bfdc1f89bf07788fbd41c3
root <root@cd78aed8941a>
2020-10-04 19:46:24 +0000

    game

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,18 +1,21 @@
 import java.awt.Color;
+import java.awt.Graphics;
 
-public class Obstacle  extends GameObject () {
+public class Obstacle  extends GameObject {
 
-	void update () {
-		
-		
+	Obstacle(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
 	}
-	
+
 	void draw(Graphics g) {
 	      g.setColor(Color.YELLOW);
 	        g.fillRect(x, y, 50, 50);
 	}
 	
-	void update () {
+	void update() {
+		y+=speed();
 		
 	}
+	
 }
