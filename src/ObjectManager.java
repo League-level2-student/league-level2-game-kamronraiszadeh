c3979a2c7b10a82962a72b787eed874bbfaa1d84
root <root@da8fdd4a1534>
2021-01-24 20:52:39 +0000

    model manegment 5

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 ArrayList<Obstacle> o=new ArrayList<Obstacle>();
 Player p;
-
+Random r = new Random ();
 ObjectManager (Player p) {
 
 	
 }
-	
+void addObstacle(){
+	o.add(new Obstacle(r.nextInt(Tylerstache.WIDTH),0,50,50));
+}
 }
